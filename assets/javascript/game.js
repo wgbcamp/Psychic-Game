adf93214c4ff61b63f7d9b2491c0a9f0df3f9d1c
Warren Brown <warrengbrn.gmail.com>
2019-11-24 00:48:51 -0500

    enabled game to start over when too many wrong guesses are attempted or a win against the computer is achieved

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,56 @@
-//hooks into win/loss & remaining guesses fields
+//these variables hook into html
 var winChanger = document.getElementById("winCounter");
 var lossChanger = document.getElementById("lossCounter");
 var guessChanger = document.getElementById("remainingGuesses");
 var guessDisplayer = document.getElementById("currentGuesses");
 
-//tracks win/loss status & guesses left
+//these variables keep track of wins, losses, and guesses remaining
 var winCount = 0;
 var lossCount = 0;
 var guessesLeft = 9;
 
+//these variables are all the characters that can be picked by computer
+var characters = 'abcdefghijklmnopqrstuvwxyz';
 
-
-
-
+//this function starts on page load
 computerGuess();
 
-//generates computer choice
+//this function causes computer to pick random number
 function computerGuess(){
-var characters = 'abcdefghijklmnopqrstuvwxyz';
-var computerChoice = characters.charAt(Math.floor(Math.random() * characters.length));
-console.log("the computer chose " + computerChoice);
-
-//keypress function
-document.addEventListener("keypress", function(event){
+    computerChoice = characters.charAt(Math.floor(Math.random() * characters.length));
     
-    //tracks user input
-    var trackUser=String.fromCharCode(event.keyCode); 
-    console.log("you pressed " + trackUser);
+}
 
-    //adds to winCount if true
-    if(computerChoice === trackUser){
-        winCount ++;
+//whenever a key is pressed, it will be recorded
+document.addEventListener("keypress", function(event){
+    trackUser=String.fromCharCode(event.keyCode);
+    
+
+//if computer and user input match, win is achieved and guesses reset, computerGuess function starts again
+    if(computerChoice==trackUser){
+        winCount++;
         winChanger.textContent = "Wins: " + winCount;
-
         guessesLeft = 9;
         guessChanger.textContent = "Guesses Left: " + guessesLeft;
         guessDisplayer.textContent = "Your Guesses so far: ";
-        
-        
-        
-    //adds to lossCount, reduces guessesLeft, and displays currentGuesses if false
-    } else {
-       
+        computerGuess();
+    }
+    //if user guesses wrong, 1 guess remaining is removed and character picked is recorded
+    else {
         guessesLeft --;
         guessChanger.textContent = "Guesses Left: " + guessesLeft;
         guessDisplayer.append(trackUser + ", ")
-
+    //if user runs out of guesses, loss is added and guesses reset, computerGuess function starts again
         if(guessesLeft==0){
             lossCount ++;
             lossChanger.textContent = "Losses: " + lossCount;
             guessesLeft = 9;
             guessChanger.textContent = "Guesses Left: " + guessesLeft;
             guessDisplayer.textContent = "Your Guesses so far: ";
+            computerGuess();
         }
     }
+})
 
-   
-});
-}
 
 
